5381491f5e8487dc8218b46feccf07a479243f49
JingLi <lijing1429@outlook.com>
2022-03-23 14:46:16 +0000

    update mysql

--- 3 Python excises/01 Sql Connect/mysql/sqltest.sql
+++ 3 Python excises/01 Sql Connect/mysql/sqltest.sql
@@ -20,9 +20,28 @@ show tables in mevaluation; */
 /* select * from mevaluation.module_result where candidaNumber is not null; */
 /* select * from mevaluation.module_result where studentMarks limit 3; */
 /* /* select * from mevaluation.users; */
-select * from Hud_BS.users; 
+/* select * from Hud_BS.bs_login;  */
+/* drop table testdata; */
+/* SELECT * FROM Hud_BS.bs_login e2 GROUP BY u_UserID,uas_SCE_Course_Code, startime HAVING COUNT(*) > 1 */
+
+/* SELECT * FROM hud_bs.bs_login LIMIT 1000; */
+/* select count(*) from Hud_BS.bs_login; 
+ALTER TABLE bs_login MODIFY uas_SCE_Course_Code varchar(255); */
+
+
+ALTER TABLE Hud_BS.bs_login ADD PRIMARY KEY(u_UserID,uas_SCE_Course_Code, startime); */
+commit; 
+/* ALTER TABLE Hud_BS.users MODIFY startime DATE;
+commit; */
 /* CREATE PROCEDURE mevaluation.Moduleresult @SPRcode nvarchar(30)
 AS
 SELECT * FROM mevaluation.module_result WHERE SPRcode = @SPRcode
 GO;
 EXEC Moduleresult @SPRcode = '123458'; */
+
+
+/* DELETE e.*FROM Hud_BS.bs_login e WHERE u_UserName IN (select id from (
+    SELECT min(u_UserName) as id
+          FROM Hud_BS.bs_login e2
+          GROUP BY u_UserID,uas_SCE_Course_Code, startime
+          HAVING COUNT(*) > 1) f); */

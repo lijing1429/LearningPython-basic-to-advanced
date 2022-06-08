c0c0b1c9c05aeacb75621d539db4cd22ed6fc295
JingLi <lijing1429@outlook.com>
2022-06-08 11:05:47 +0100

    updates

--- 3 Python excises/03 Data processing/planning.py
+++ 3 Python excises/03 Data processing/planning.py
@@ -1,9 +1,26 @@
+from importlib.machinery import PathFinder
 import pandas as pd
+import numpy as np
 
-path = r'C:\Users\SHUMJL7\OneDrive - University of Huddersfield\Documents\002 data project\007 course progression\data report\age.xlsx'
+path = r'C:\Users\Staff\OneDrive - University of Huddersfield\Documents\002 data project\007 course progression\data report\20211028 Progression Tables - HHS.xlsx'
+xls_file = pd.ExcelFile(path)
+# print(xls_file.sheet_names)
+for sheetname in xls_file.sheet_names:
+    df_new = pd.DataFrame()
+    print(sheetname)  # 1. student characteristics
 
-Sexdata = pd.read_excel(path,sheet_name= 'sex')
+    df = pd.read_excel(path,sheet_name=sheetname)
+    start_num = df.loc[df.iloc[:,0]=='School'].index.tolist()[0]    #get the row index of data starts
 
-print(Sexdata)
+    # print(df.iloc[start_num+1:,0:5].to_string(header=False))    #get the course details
 
-# add new rows - 'gap' according to different sex type: KPI attainment of gap = male - female (same year, same course and same route)
+    acy = df.iloc[start_num-2,5]
+    df_new['year']= acy
+    
+    df_new['gap'] = df.iloc[start_num+1:,6]-df.iloc[start_num+1:,8]
+    print(df_new)
+
+    # print(df.iloc[start_num:,6]-df.iloc[start_num:,7])
+    break
+    
+# print(df)

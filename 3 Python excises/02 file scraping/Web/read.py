3d00d3c9a68f49c58e6c4346207ad0a1b56ab904
JingLi <lijing1429@outlook.com>
2022-12-19 15:42:04 +0000

    updates web scrapping

--- 3 Python excises/02 file scraping/Web/read.py
+++ 3 Python excises/02 file scraping/Web/read.py
@@ -1,8 +1,42 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://www.po18vip.uk/read/58319/"
+def url_pro(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    return soup.get_text().replace("\n\n", "")
+
+URL = "https://www.haitang16.com/book/62941.html"
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, "html.parser")
-print(soup)
+results = soup.find("ol", class_="inline mb20")
+
+l1 = []
+
+for ele in results.find_all("li"):
+
+    url = ele.find("a").get('href')
+    l1.append(ele.get_text())
+    l1.append(url_pro(url))
+    print(1)
+    # print(ele.get_text())
+    # break
+
+with open("file12.txt", "w", encoding='utf-8') as output:
+    for row in l1:  
+        output.write(str(row) + '\n')
+        print(2)
+
+# print(l1)
+    
+    
+    # url = 'http://m.marchettieng.com'+ ele.find("a").get('href')
+    # print(url)
+
+
+
+# # print(soup)
+# print(results)
+# print(len(results))
+# print(type(soup))
